93fcc58cd6a712a78baa6c6f2c507817ac252ffa
shurik <loban.sasha.97@mail.ru>
2020-08-22 16:16:33 +0300

    tests: to be continued

--- spec/views/info_pages/about.html.erb_spec.rb
+++ spec/views/info_pages/about.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "info_pages/about.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is it a toad?" do
+    # expect("info_pages/about").to have_css("img[src*='pepeinst.jpeg']") # FIXED
+    # expect(img_tag["src"]).to eq "pepeinst.jpeg" # FIXED
+  end
+  
 end
