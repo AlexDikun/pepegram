363425625ab55a5f7a80ba74d76232971159ccf0
shurik <loban.sasha.97@mail.ru>
2020-08-31 11:22:40 +0300

    fixing tests due to routing

--- spec/requests/public_request_spec.rb
+++ spec/requests/public_request_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe "Publics", type: :request do
 
   describe "GET /home" do
     it "returns http success" do
-      get "/public/home"
+      get "/home"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /help" do
     it "returns http success" do
-      get "/public/help"
+      get "/help"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /about" do
     it "returns http success" do
-      get "/public/about"
+      get "/about"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /contact" do
     it "returns http success" do
-      get "/public/contact"
+      get "/contact"
       expect(response).to have_http_status(:success)
     end
   end
