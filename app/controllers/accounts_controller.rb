c9716b9b8fde9a9529bf8cf6477d9841c594aa3b
shurik <loban.sasha.97@mail.ru>
2020-09-09 20:52:25 +0300

    create new form registrations - added key 'username' for account

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,25 @@
+class AccountsController < ApplicationController
+
+  def show
+    @acc = Account.find(params[:id])
+  end
+
+  def edit
+    @acc = Account.find(params[:id])
+  end
+
+  def update
+    @acc = Account.find(params[:id])
+    if account.update(account_update_params)
+     redirect_to account_path(@acc)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def account_update_params
+    params.require(:account).permit(:username)
+  end
+end
