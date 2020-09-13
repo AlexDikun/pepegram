82a3988c87702bed21f3e8445282696b5fe4886e
shurik <loban.sasha.97@mail.ru>
2020-09-13 22:13:40 +0300

    write tests for password validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,13 @@
 class Account < ApplicationRecord
-  EMAIL_FORMAT = /\A[^@\s]+@[^@\s]+\z/
-
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  PASSWORD_FORMAT = /\A(?=.*[a-z])(?=.*[\d]).{8,}\z/i
+  
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: EMAIL_FORMAT, message: "Invalid email!"}
-  validates :username, presence: true, length:{ maximum: 30 }
+  validates :username, presence: true, length: { maximum: 30 }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, format: { with: PASSWORD_FORMAT,
+                                 message: "Invalid password!"}
 
   has_many :posts
 
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Account, type: :model do
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_presence_of(:password) }
 end
