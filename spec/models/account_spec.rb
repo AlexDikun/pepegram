802280487675b3d2d343d20a509ad0cbc18bce72
shurik <loban.sasha.97@mail.ru>
2020-09-16 14:56:06 +0300

    testsis working

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe Account, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to validate_presence_of(:password) }
 
-  it { is_expected.to have_many(:follower_follows).with_foreign_key(:following_id.class_name('Follow') }
+  it { is_expected.to have_many(:follower_follows).with_foreign_key(:following_id).class_name('Follow') }
   it { is_expected.to have_many(:followers).through(:follower_follows).source(:follower) }
 
   it { is_expected.to have_many(:following_follows).with_foreign_key(:follower_id).class_name('Follow') }
-  it { is_expected.to have_many(:followings).through(:followings_follows).source(:following) }
+  it { is_expected.to have_many(:followings).through(:following_follows).source(:following) }
 
 end
