6d50e13813e6d3518e8e8413863625c76ef816c1
shurik <loban.sasha.97@mail.ru>
2020-09-19 17:11:37 +0300

    feature/Follow-Unfollow/ is working

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,22 +1,23 @@
 # app/controllers/follows_controller.rb
 # frozen_string_literal: true
 
+
 class FollowsController < ApplicationController
   before_action :authenticate_account!
 
   def create
-    follower_acc = current_account
     following_acc = Account.find(params[:following_id])
 
-    Follow.create(follower: follower_acc, following: following_acc)
+    Follow.create(follower: current_account, following: following_acc)
     redirect_to account_posts_path(following_acc),
       flash: {success: 'You have successfully subscribed'}
   end
 
   def destroy
-    @follow = Follow.find(params[:id])
-    # @follow = Follow.where('account_id = ? AND follows = ?',
-    #                        current_account.id, params[:account_id])
+    following_acc = Account.find(params[:following_id])
+
+    @follow = Follow.find_by(follower_id: current_account.id,
+                             following_id: following_acc.id)
     @follow.destroy
     redirect_to account_path(current_account),
       flash: {success: 'You have successfully unfollow'}
