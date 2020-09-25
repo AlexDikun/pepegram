915baaccac976b7678912075e0c29cd83fd2e0e7
shurik <loban.sasha.97@mail.ru>
2020-09-26 00:39:42 +0300

    test unit for action Destroy from FollowsController is written

--- spec/controllers/follows_controller_spec.rb
+++ spec/controllers/follows_controller_spec.rb
@@ -7,16 +7,11 @@ RSpec.describe FollowsController, type: :controller do
   let!(:first_user)  { create :account }
   let!(:second_user) { create :account }
 
+  # current_account
   before { sign_in first_user }
-  before { sign_in second_user }
 
   describe 'create' do
-    let(:params) do
-      {
-        follower_id:  first_user.id,
-        following_id: second_user.id,
-      }
-    end
+    let(:params) { { following_id: second_user.id } }
 
     subject { post :create, params: params }
 
@@ -24,6 +19,18 @@ RSpec.describe FollowsController, type: :controller do
       expect { subject }.to change { Follow.count }.by(1)
       # is_expected.to redirect_to(account_posts_path(assigns(:second_user))) # what do not work?
       is_expected.to redirect_to(account_posts_path(second_user))
+    end
+  end
+
+  describe 'destroy' do
+    let!(:follow) { Follow.create(follower: first_user, following: second_user) }
+    let(:params) { { following_id: second_user.id, id: follow.id } }
+
+    subject { delete :destroy, params: params }
+
+    it 'destroy follow' do
+      expect { subject }.to change { Follow.count }.by(-1)
+      is_expected.to redirect_to(account_path(first_user))
     end
   end
 end
