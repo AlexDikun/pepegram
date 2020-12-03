1942ed36c3bb2b93ea559ab46a71b55a87b73a9d
Alex <loban.sasha.97@mail.ru>
2020-12-03 19:15:50 +0300

    there is a restriction on the format of the loaded images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,13 @@
 # top-level class documentation comment
 class ImageUploader < Shrine
   plugin :default_url
+  plugin :validation_helpers
 
   Attacher.default_url do |**options|
     '/placeholders/frogg.jpeg'
+  end
+
+  Attacher.validate do
+    validate_extension %w[jpg jpeg gif png svg webp]
   end
 end
