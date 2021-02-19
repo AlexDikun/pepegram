67b062c84782cfd54cacd47da687bf6a4afe9116
Alex <loban.sasha.97@mail.ru>
2021-02-19 17:33:14 +0300

    tests for accounts_controller are done

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -17,4 +17,36 @@ RSpec.describe AccountsController, type: :controller do
 
     it { is_expected.to render_template(:show) }
   end
+
+  describe 'edit' do
+    subject { process :edit, method: :get, params: params }
+
+    it { is_expected.to render_template(:edit) }
+
+    it 'assigns server policy' do
+      subject
+      expect(assings(:acc)).to eq(user)
+    end
+  end
+
+  describe 'update' do
+    let!(:user) { create :account }
+    let(:params) { { id: user.id, user: { username: 'Navalny' } } }
+
+    subject { process :update, method: :put, params: params }
+
+    it { is_expected.to redirect_to(account_path(assigns(:acc))) }
+
+    it 'updates account' do
+      expect { subject }.to change { user.reload.username }.to('Navalny')
+    end
+
+    context 'with bad params' do
+      let(:params) { { id: user.id, user: { username: ' ' } } }
+
+      it 'does not update account' do
+        expect { subject }.not_to change { user.reload.username }
+      end
+    end
+  end
 end
