b180ab08dd500091346fe6c7da1109671fa24886
Alex <loban.sasha.97@mail.ru>
2021-02-25 15:55:54 +0300

    tests for the Post model are written

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:account) }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_length_of(:title).is_at_most(100) }
+  it { is_expected.to validate_presence_of(:message) }
+  it { is_expected.to validate_presence_of(:image_data) }
+
+  context 'validation image format' do
+    it 'allows to set png file as an image' do
+      user = create(:account)
+      subject.attributes = attributes_for(:post)
+      subject.account = user
+      is_expected.to be_valid
+    end
+
+    it 'disallows to set png file as an image' do
+      user = create(:account)
+      subject.attributes = attributes_for(:post, :with_invalid_image)
+      subject.account = user
+      is_expected.to be_invalid
+    end
+  end
 end
