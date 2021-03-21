a8d0cc53daf5d63e237d0e645e1a83ad4b2a43c0
Alex <loban.sasha.97@mail.ru>
2021-03-21 21:41:27 +0300

    test for public_controller are written

---
+++ spec/controllers/public_controller_spec.rb
@@ -0,0 +1,19 @@
+# spec/controllers/public_controller_spec.rb
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PublicController, type: :controller do
+  let(:account) { create :account }
+
+  describe 'list' do
+    subject { get :list }
+
+    it 'assigns @accounts' do
+      subject
+      expect(assigns(:accounts)).to eq([account])
+    end
+
+    it { is_expected.to render_template('list') }
+  end
+end
