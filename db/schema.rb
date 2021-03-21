8d4b08f2f2f1c7bb8eb97a6bbe4a8fa766fc23f6
Alex <loban.sasha.97@mail.ru>
2021-03-21 20:51:18 +0300

    it's a refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -18,19 +18,8 @@ ActiveRecord::Schema.define(version: 2021_03_02_200455) do
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
@@ -44,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_200455) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "following_id"
+    t.index ["follower_id", "following_id"], name: "index_follows_on_follower_id_and_following_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
