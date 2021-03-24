b4d1bea0a58fd6ad587fe1af1e87f43c87b9b05e
Alex <loban.sasha.97@mail.ru>
2021-03-24 09:33:47 +0300

    fix shrine warnning

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -12,3 +12,4 @@ Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
 Shrine.plugin :restore_cached_data
 Shrine.plugin :derivatives, create_on_promote: true
+Shrine.plugin :determine_mime_type
